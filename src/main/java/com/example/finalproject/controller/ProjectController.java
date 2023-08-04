5c87dad5edc3e022cfa597fa3ca8c2bcfc48fadb
Luonghaison <keibin.wru@gmail.com>
2023-08-04 14:00:50 +0700

    fix:fix template 2nd time

--- src/main/java/com/example/finalproject/controller/ProjectController.java
+++ src/main/java/com/example/finalproject/controller/ProjectController.java
@@ -127,7 +127,7 @@ public class ProjectController {
     public String index(@RequestParam(required = false, defaultValue = "") String textSearch, Pageable pageable, Model model) {
         Page<EmployeeDTO> listOfEmployees = employeeService.findAll(textSearch, pageable);
         model.addAttribute("listOfEmployees", listOfEmployees);
-        return "project/employee_create";
+        return "project/create-employee";
     }
 
 
@@ -136,7 +136,7 @@ public class ProjectController {
     public ModelAndView doAdd(@RequestParam(value = "selectedEmployeeCodes", required = false) List<String> selectedEmployees,
                               HttpSession session, RedirectAttributes redirectAttributes) throws Exception {
         if (selectedEmployees == null || selectedEmployees.isEmpty()) {
-            ModelAndView modelAndView = new ModelAndView("project/employee_create");
+            ModelAndView modelAndView = new ModelAndView("project/create-employee");
             return modelAndView;
         }
 
